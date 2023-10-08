5ccfd65baac3caf2f55489f85256ab013da51c50
Amit Kumar <119536812+Amit-10101@users.noreply.github.com>
2023-10-08 22:14:48 +0530

    Initial Commit

---
+++ Lecture-07_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int password;
+	int count = 0;
+	//	while(1){
+	//		cout<<"choclates ";
+	//		count++;
+	//		if(count == 100){
+	//			break;
+	//		}
+	//	}
+
+	for (int i = 0;; i++)
+	{
+		//    	cout<<"amit"<<" ";
+		cout << i << " ";
+		if (i > 100)
+			break;
+	}
+
+	return 0;
+}
